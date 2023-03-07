163caadcb1c8d33b86152a75cc14bbfe4876ba09
jwansek <eddie.atten.ea29@gmail.com>
2023-03-07 23:49:54 +0000

    Updated dockerfile

--- ros/entrypoint.sh
+++ ros/entrypoint.sh
@@ -1,7 +1,8 @@
 source /opt/ros/humble/setup.bash
 tmux new-session -d -s "fastdds" 'bash -ic "bash /mower/fastdds-tmux.sh" || bash && bash';
 
-source /mower/LocationPublisher/install/setup.bash 
+source /mower/telemetry/install/setup.bash 
 export ROS_DISCOVERY_SERVER=localhost:11811
 export ROS2_DOMAIN_ID=142
-ros2 run loc_updater loc_reciever
+# ros2 run loc_updater loc_reciever
+ros2 topic echo top_nmea
